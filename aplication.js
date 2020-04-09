2ebb3cdb55d0681623a3bbb4b30f562d2cd9b6c2
Juan Carlos Mendoza García <carlos4561059919@gmail.com>
2020-04-08 22:53:46 -0500

    version1

--- aplication.js
+++ aplication.js
@@ -1,31 +1,22 @@
-//Cuando necesitamos hacer uso de una fuction, en otras areas de nuestro programa, código, etc
-//Lo mejor es heredar esa funcionalidad, en lugar de copear todo el código.
-function Alumnos(nombre, grado){
-    this.nombre= nombre;
-    this.grado= grado;
- 
+//Funcion Object create
+//Clase
+const Cliente = {
+    //Métodos o prototipes 
+    imprimirSaldo: function () {
+        return `Hola ${this.nombre} tu saldo es ${this.saldo}`
+    },
+    retirarSaldo: function (retiro) {
+        return this.saldo -= retiro;
+    }
 }
-//Ejemplo de prototipe
-Alumnos.prototype.gradoClientenombre= function(){
-    return `Nombre: ${this.nombre}, Tu grado es: ${this.grado}`;
-}
-const alumno1= new Alumnos('juan', 'primero');
-console.log(alumno1);
+// Creacion de un objeto
+const juan = Object.create(Cliente);
+juan.nombre = 'Juan';
+juan.saldo = 1000;
 
+//Realizamos un retiro instanciando al objeto y llamando al método
+juan.retirarSaldo(500);
 
+console.log(juan.imprimirSaldo());
 
-//En esta otra fuction, queremos usar parametro de la fuction Alumnos, por ello heredaremos
-function Escuela(nombre, grado, direccion, tipo){
-Alumnos.call(this, nombre, grado);
-this.direccion=direccion;
-this.tipo=tipo;
-} // Llamamos a la clase. metodo call y le pasamos los parametros que necesitamos
-
-//Como heredamos un prototype, para poer usarlo en otra fuction?
-Escuela.prototype= Object.create(Alumnos.prototype); //Esta linea debe ir arriba, por que si no se añadira primero la instancia y luego se añade el prototipo
-
-const escuel= new Escuela('Benito Juares','seis', 'Mexico','Musica',); //Instnacia
-console.log(escuel.gradoClientenombre());
-//De esta manera podemos crear protitypes en una clase principal "Alumnos"
-// e irlos heredando y la funcionalidad la podremos reutilizar, con información propia de la clase.
-
+//Desarrollado en base a la estructura de ES5 considerada la programación a objetos
